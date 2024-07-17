1de487bb7befe5ae232cae9f79b8ba26b8d1c9fe
zhangnew <9146834+zhangnew@users.noreply.github.com>
2024-07-17 14:14:50 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.zhangnew.com": dockerHub,
+  "quay.zhangnew.com": "https://quay.io",
+  "gcr.zhangnew.com": "https://gcr.io",
+  "k8s-gcr.zhangnew.com": "https://k8s.gcr.io",
+  "k8s.zhangnew.com": "https://registry.k8s.io",
+  "ghcr.zhangnew.com": "https://ghcr.io",
+  "cloudsmith.zhangnew.com": "https://docker.cloudsmith.io",
+  "ecr.zhangnew.com": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.zhangnew.com": dockerHub,
 };
 
 function routeByHosts(host) {
